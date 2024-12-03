863cf7adcf85015f5c4979183d96994f4970ee53
sivaguru13 <155611778+sivaguru13@users.noreply.github.com>
2024-12-03 12:12:21 +0530

    app

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
-
+import Homepage from './Components/Homepage';
 function App() {
-    
+    return(
+        <BrowserRouter>
+        <Routes>
+            <Route path='/home' element={<Homepage/>}/>
+            </Routes>
+        </BrowserRouter>
+    );
 }
 
 export default App;
---
+++ src/Components/homepage.js
@@ -0,0 +1,14 @@
+import { Button } from "@mui/material";
+
+function Homepage(){
+    return(
+        <div>
+            <h1>Welcom to Dashboard</h1>
+            <div><Button variant="contained">Contained</Button>
+            <Button variant="contained">Contained</Button> </div>
+        </div>
+    );
+
+}
+
+export default Homepage;
