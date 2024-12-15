23160892615b037d95c1d65a261ece0a5022f0fa
sivaguru13 <155611778+sivaguru13@users.noreply.github.com>
2024-12-15 23:02:23 +0530

    dashboard design correction

--- src/Components/Dashboard.js
+++ src/Components/Dashboard.js
@@ -59,6 +59,12 @@ import { useState, useEffect } from "react";
 import "./Dashboard.css";
 import LogoutIcon from '@mui/icons-material/Logout';
 import { useNavigate } from "react-router-dom";
+import MenuIcon from '@mui/icons-material/Menu';
+import { Button } from "@mui/material";
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import ShoppingBagIcon from '@mui/icons-material/ShoppingBag';
+import Person2Icon from '@mui/icons-material/Person2';
+import CurrencyRupeeIcon from '@mui/icons-material/CurrencyRupee';
 
 function Dashboard() {
     const [step, setStep] = useState(1); // Controls which page/section to display
@@ -90,6 +96,18 @@ function Dashboard() {
     }
     }, []);
 
+    //menubar
+    const menuClick = () =>{
+      // navigate('/signin')
+      if(document.getElementById('sidebar').style.display==="flex"){
+        document.getElementById('sidebar').style.display="none"
+        document.getElementById('rightside').style.width="100%"
+      }
+      else{
+        document.getElementById('sidebar').style.display="flex"
+      }
+    }
+
     const logOut = () => {
     localStorage.removeItem("email");
     navigate('/signin');
@@ -98,12 +116,16 @@ function Dashboard() {
     return (
     <div className="whole">
       {/* Sidebar */}
-        <div className="sidebar">
+     <Button onClick={menuClick} style={{height:"30px",padding:"0px"}}><MenuIcon className="menu-icon" style={{position:"absolute",left:"10",top:"10"}} /></Button>
+     
+        <div id="sidebar" >
+        
         <img
             src="https://static.vecteezy.com/system/resources/previews/005/544/770/original/profile-icon-design-free-vector.jpg"
             alt="Profile"
         />
         <h1 id="msg">Welcome {email}</h1>
+       
         <div className="nav-left">
           {/* Buttons to change step */}
             <button onClick={() => setStep(1)} className="butns">
@@ -118,6 +140,7 @@ function Dashboard() {
             <button onClick={() => setStep(4)} className="butns">
             SalesReports
             </button>
+            
         </div>
         </div>
 
@@ -130,7 +153,16 @@ function Dashboard() {
         <div id="rightside">
         {step === 1 && 
         <div>
-            
+            <div className="wholecontent">
+              <div className="dashboards content1">
+                <h3 style={{color: "rgb(59, 103, 9, 1)"}}><div className="icondiv icn1"><ShoppingCartIcon style={{fontSize:"2pc"}}/></div>Orders</h3></div>
+              <div  className="dashboards content2">
+              <h3 style={{color:"rgb(6, 7, 96, 1)"}}><div className="icondiv icn2"><ShoppingBagIcon style={{fontSize:"2pc"}}/></div>Inventory</h3></div>
+              <div  className="dashboards content3">
+              <h3 style={{color:"rgb(6, 7, 96, 1)"}}><div className="icondiv icn3">< Person2Icon  style={{fontSize:"2pc"}}/></div> Customer</h3></div>
+              <div  className="dashboards content4">
+              <h3 style={{color:"rgb(119, 5, 14, 1)"}}><div className="icondiv icn4"><CurrencyRupeeIcon style={{fontSize:"2pc"}}/></div> Revenue</h3></div>
+            </div>
         </div>}
         {step === 2 && 
             <div>
