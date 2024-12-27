9d4645ad8514b17884168fe60eb6aa2923ea9060
sivaguru13 <155611778+sivaguru13@users.noreply.github.com>
2024-12-27 11:55:39 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
-import Homepage from './Components/Homepage';
+import homepage from './Components/homepage';
 import Login from './Components/Signin';  
 import Signup from './Components/signup';
 import SellerDashboard from './Components/SellerDashboard';
@@ -15,7 +15,7 @@ function App() {
     return(
         <BrowserRouter>
         <Routes>
-            <Route path='/home' element={<Homepage/>}/>
+            <Route path='/home' element={<homepage/>}/>
             <Route path='/signin' element={<Login/>}/>
             <Route path='/signup' element={<Signup/>}/>
             <Route path="" element={<SellerDashboard/>}/> 
